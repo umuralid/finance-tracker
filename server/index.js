3e6e72a352816a1b33c6be051353ec77967ff8c8
umuralid <umuralid@amazon.com>
2026-06-09 09:21:37 +0530

    fix BigInt serialization

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const db = require('./db');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.set('json replacer', (key, value) => typeof value === 'bigint' ? Number(value) : value);
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 // Signup
